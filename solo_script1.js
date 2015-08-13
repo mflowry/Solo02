0b9cbf3ffd7a3734e9365218ad2ba0ccfba226c6
Madeleine Lowry <mlowry55403@gmail.com>
2015-08-12 21:29:31 -0500

    first commit

--- solo_script1.js
+++ solo_script1.js
@@ -1,5 +1,8 @@
 // ! ! !
-// Three Bugs
+// Three Bugs:
+// 1. The newArray is iterating over Atticus each time because the function calculatesSTI always starts at index 0.
+// 2. DOM is displaying the original array, instead of the final array.
+// 3. Bonus is being calculated incorrectly (getBaseSTI removed -1).
 
 var arrayAtticus = ["Atticus", "2405", "47000", 3];
 var arrayJem = ["Jem", "62347", "63500", 4];
@@ -7,6 +10,11 @@ var arrayBoo = ["Boo", "11435", "54000", 3];
 var arrayScout = ["Scout", "6243", "74750", 5];
 
 var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+var finalArray = [];
+
+array.forEach(function(array) {
+  calculateSTI(array);
+})
 
 //Create variables used to write to the DOM
 var newEl, newText, position;
@@ -15,33 +23,43 @@ position = document.getElementById('content');
 
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
-for(var i = 0; i < array.length; i++){
-	array[i] = calculateSTI(array);
- 	newEl = document.createElement('li');
-	newText = document.createTextNode(array[i]);
-	newEl.appendChild(newText);
-	position.appendChild(newEl);
+
+
+for(var i = 0; i < finalArray.length; i++){
+  newEl = document.createElement('li');
+  newText = document.createTextNode(finalArray[i]);
+  newEl.appendChild(newText);
+  position.appendChild(newEl);
 }
 
 function calculateSTI(array){
   var newArray = [];
-
   newArray[0] = array[0];
 
   var employeeNumber = array[1];
-  var baseSalary = array[2];
-  var reviewScore = array[3];
+  var baseSalary = parseInt(array[2]);
+  var reviewScore = parseInt(array[3]);
+  console.log(employeeNumber, baseSalary, reviewScore);
 
   var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+
+  console.log(getBaseSTI(reviewScore), getYearAdjustment(employeeNumber), getIncomeAdjustment(baseSalary));
+
+
   if(bonus > 0.13){
     bonus = 0.13;
+    console.log(bonus);
   }
-
+ 
   newArray[1] = bonus;
-  newArray[2] = baseSalary * (1.0 + bonus);
-  newArray[3] = baseSalary * bonus;
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus));
+  newArray[3] = Math.round(baseSalary * bonus);
   console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+
+  finalArray.push(newArray);
+  console.log(finalArray);
   return newArray;
+
 }
 
 function getBaseSTI(reviewScore){
@@ -63,7 +81,7 @@ function getBaseSTI(reviewScore){
       basePercent = 0.10;
       break;
   }
-  return basePercent - 1;
+  return basePercent; 
 }
 
 function getYearAdjustment(employeeNumber){
